b69423454477540aefba0e1fdc5ee9ceb8b8aff3
artemijafanasev <artemiys117@mail.ru>
2022-08-10 16:48:52 +0300

    Start new kata

---
+++ vowels.java
@@ -0,0 +1,10 @@
+public class vowels {
+    public static String disemvowel(String str) {
+
+        return str;
+    }
+    public static void main(String[] args) {
+        System.out.println(disemvowel("bruh looser"));
+    }
+}
+
