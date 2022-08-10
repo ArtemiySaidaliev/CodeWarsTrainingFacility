d16037a67748b86f58ee5fb2b08f40446b4f1fe4
artemijafanasev <artemiys117@mail.ru>
2022-08-10 20:19:57 +0300

    start jojo kata

---
+++ jojo.py
@@ -0,0 +1,11 @@
+#you can either create a valid regex to find if a character
+#is a JoJo or create a function for it.
+#bonus points if you do both ;)
+
+#Oh, and think about beginners: try to be informative and
+#comment with some explanation for the sake of their learning!
+
+regex="" #your regex here or...
+
+def is_jojo(name):
+    pass #...your code here - do not delete either one even if not used
