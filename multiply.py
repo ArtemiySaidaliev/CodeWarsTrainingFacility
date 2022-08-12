7687c5312bf3bb2830996459cdb17af9d1f2f0a0
artemijafanasev <artemiys117@mail.ru>
2022-08-12 20:55:08 +0300

    Optimise version with o(1)

--- multiply.py
+++ multiply.py
@@ -1,16 +1,19 @@
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally,
+if the number is negative, return 0 (for languages that do have them).
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+
+
+O(1) solution:
+"""
+
 def solution(number):
-    i = 0
-    sum = 0
-    while i < number:
-        sum+=i
-        i+=5
-    i = 0
-    while i < number:
-        if i % 5 != 0:
-            sum+=i
-        i+=3
-    return sum
-    pass
-
-
+    a3 = (number - 1) / 3
+    a5 = (number - 1) / 5
+    a15 = (number - 1) / 15
+    result = (a3 * (a3 + 1) / 2) * 3 + (a5 * (a5 + 1) / 2) * 5 - (a15 * (a15 + 1) / 2) * 15
+    return result
 print(solution(5))
