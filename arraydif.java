d008adc752eb52dc8e11135901de5356f308865e
artemijafanasev <artemiys117@mail.ru>
2022-08-20 18:11:09 +0300

    working but not optimize solution

--- arraydif.java
+++ arraydif.java
@@ -7,23 +7,37 @@ It should remove all values from list a which are present in list b keeping thei
 It should remove all values from list a which are present in list b keeping their order.
  */
 
+
+import java.util.Arrays;
+
 public class arraydif {
 
     public static int[] arrayDiff(int[] a, int[] b) {
-        for (int k : a) {
-            for (int i : b) {
-                if (k == i) {
-                    System.out.println("Find you" + k + i);
+        System.out.println(a[0]);
+        System.out.println(Arrays.toString(a));
+        int del_count = 0;
+        for (int k = 0; k < a.length; k++) {
+            for (int i = 0; i < b.length; i++) {
+                if (a[k] == b[i]) {
+                    del_count++;
+                    System.out.println("Find you" + a[k] + b[i]);
+                    for(int j = k; j < a.length - 1; j++){
+                        a[j] = a[j+1];
+                    }
+                    k--;
                 }
             }
-
         }
-        return a;
+        int[] c = new int[a.length-del_count];
+        for(int i = 0; i < a.length-del_count;i++){
+            c[i] = a[i];
+        }
+        return c;
     }
     public static void main(String[] args) {
-        int[]a = {1,2,2};
-        int[]b = {2,3};
-        arrayDiff(a,b);
+        int[]a = {1,2,2,3,3,4,4,5,5,6,7};
+        int[]b = {2,3,5,6,4};
+        System.out.println(Arrays.toString(arrayDiff(a, b)));
 
     }
 
